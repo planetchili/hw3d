e6b418a7935eb49a80f026a4e8699cbc894ccb3f
chili <ray_kun@hotmail.com>
2018-11-30 13:41:43 +0900

    create and show window

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -6,6 +6,32 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow )
 {
+	const auto pClassName = "hw3dbutts";
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof( wc );
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx( &wc );
+	// create window instance
+	HWND hWnd = CreateWindowEx(
+		0,pClassName,
+		"Happy Hard Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200,200,640,480,
+		nullptr,nullptr,hInstance,nullptr
+	);
+	// show the damn window
+	ShowWindow( hWnd,SW_SHOW );
 	while( true );
 	return 0;
 }
