bb3081ad84ab6333ea2d0d221f9f948a14214606
chili <ray_kun@hotmail.com>
2018-12-09 16:11:08 +0900

    custom app icon

--- hw3d/Window.cpp
+++ hw3d/Window.cpp
@@ -19,6 +19,7 @@
 ******************************************************************************************/
 #include "Window.h"
 #include <sstream>
+#include "resource.h"
 
 
 // Window Class Stuff
@@ -35,12 +36,18 @@ Window::WindowClass::WindowClass() noexcept
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = GetInstance();
-	wc.hIcon = nullptr;
+	wc.hIcon = static_cast<HICON>(LoadImage( 
+		GetInstance(),MAKEINTRESOURCE( IDI_ICON1 ),
+		IMAGE_ICON,32,32,0
+	));
 	wc.hCursor = nullptr;
 	wc.hbrBackground = nullptr;
 	wc.lpszMenuName = nullptr;
 	wc.lpszClassName = GetName();
-	wc.hIconSm = nullptr;
+	wc.hIconSm = static_cast<HICON>(LoadImage(
+		GetInstance(),MAKEINTRESOURCE( IDI_ICON1 ),
+		IMAGE_ICON,16,16,0
+	));
 	RegisterClassEx( &wc );
 }
 
