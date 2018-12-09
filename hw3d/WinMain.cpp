e5933fa6724411e8584de83f72195fed466406ce
chili <ray_kun@hotmail.com>
2018-12-09 14:52:22 +0900

    top level try...catch with messagebox

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -26,23 +26,39 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow )
 {
-	Window wnd( 800,300,"Donkey Fart Box" );
-
-	MSG msg;
-	BOOL gResult;
-	while( (gResult = GetMessage( &msg,nullptr,0,0 )) > 0 )
+	try
 	{
-		// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
-		TranslateMessage( &msg );
-		DispatchMessage( &msg );
-	}
+		Window wnd( 800,300,"Donkey Fart Box" );
 
-	// check if GetMessage call itself borked
-	if( gResult == -1 )
+		MSG msg;
+		BOOL gResult;
+		while( (gResult = GetMessage( &msg,nullptr,0,0 )) > 0 )
+		{
+			// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
+			TranslateMessage( &msg );
+			DispatchMessage( &msg );
+		}
+
+		// check if GetMessage call itself borked
+		if( gResult == -1 )
+		{
+			return -1;
+		}
+
+		// wParam here is the value passed to PostQuitMessage
+		return msg.wParam;
+	}
+	catch( const ChiliException& e )
 	{
-		return -1;
+		MessageBox( nullptr,e.what(),e.GetType(),MB_OK | MB_ICONEXCLAMATION );
 	}
-
-	// wParam here is the value passed to PostQuitMessage
-	return msg.wParam;
+	catch( const std::exception& e )
+	{
+		MessageBox( nullptr,e.what(),"Standard Exception",MB_OK | MB_ICONEXCLAMATION );
+	}
+	catch( ... )
+	{
+		MessageBox( nullptr,"No details available","Unknown Exception",MB_OK | MB_ICONEXCLAMATION );
+	}
+	return -1;
 }
