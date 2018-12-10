1466aa8466fadf60f33ec515fd0ee0326b050c40
chili <ray_kun@hotmail.com>
2018-12-11 01:00:46 +0900

    clear keystate on focus loss

--- hw3d/Window.cpp
+++ hw3d/Window.cpp
@@ -137,6 +137,10 @@ LRESULT Window::HandleMsg( HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam ) noex
 	case WM_CLOSE:
 		PostQuitMessage( 0 );
 		return 0;
+	// clear keystate when window loses focus to prevent input getting "stuck"
+	case WM_KILLFOCUS:
+		kbd.ClearState();
+		break;
 
 	/*********** KEYBOARD MESSAGES ***********/
 	case WM_KEYDOWN:
