5fdf7c845886d9a348309dea42e1d0f295cbfcdb
chili <ray_kun@hotmail.com>
2018-12-24 12:03:33 -0700

    test stuff

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -18,7 +18,7 @@
 *	along with The Chili Direct3D Engine.  If not, see <http://www.gnu.org/licenses/>.    *
 ******************************************************************************************/
 #include "Window.h"
-
+#include <sstream>
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -37,6 +37,25 @@ int CALLBACK WinMain(
 			// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
 			TranslateMessage( &msg );
 			DispatchMessage( &msg );
+
+			// test code
+			while( !wnd.mouse.IsEmpty() )
+			{
+				const auto e = wnd.mouse.Read();
+				switch( e.GetType() )
+				{
+				case Mouse::Event::Type::Leave:
+					wnd.SetTitle( "Gone!" );
+					break;
+				case Mouse::Event::Type::Move:
+					{
+						std::ostringstream oss;
+						oss << "Mouse moved to (" << e.GetPosX() << "," << e.GetPosY() << ")";
+						wnd.SetTitle( oss.str() );
+					}
+					break;
+				}
+			}
 		}
 
 		// check if GetMessage call itself borked
