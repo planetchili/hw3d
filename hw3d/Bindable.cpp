e238386c78a9ddf9d3e3f62d193cc918f946d7c3
chili <ray_kun@hotmail.com>
2019-02-15 14:36:04 +0900

    fix noexcept spec

--- hw3d/Bindable.cpp
+++ hw3d/Bindable.cpp
@@ -10,7 +10,7 @@ ID3D11Device* Bindable::GetDevice( Graphics& gfx ) noexcept
 	return gfx.pDevice.Get();
 }
 
-DxgiInfoManager& Bindable::GetInfoManager( Graphics& gfx ) noexcept(!IS_DEBUG)
+DxgiInfoManager& Bindable::GetInfoManager( Graphics& gfx ) noexcept(IS_DEBUG)
 {
 #ifndef NDEBUG
 	return gfx.infoManager;
--- hw3d/Bindable.h
+++ hw3d/Bindable.h
@@ -9,5 +9,5 @@ public:
 protected:
 	static ID3D11DeviceContext* GetContext( Graphics& gfx ) noexcept;
 	static ID3D11Device* GetDevice( Graphics& gfx ) noexcept;
-	static DxgiInfoManager& GetInfoManager( Graphics& gfx ) noexcept(!IS_DEBUG);
+	static DxgiInfoManager& GetInfoManager( Graphics& gfx ) noexcept(IS_DEBUG);
 };
