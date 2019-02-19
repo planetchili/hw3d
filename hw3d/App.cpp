d0b343785d0cc64c68a4d1101b3401e6b3be9d29
chili <ray_kun@hotmail.com>
2019-02-19 14:30:52 +0900

    restore cube test

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -24,11 +24,6 @@ void App::DoFrame()
 	const float c = sin( timer.Peek() ) / 2.0f + 0.5f;
 	wnd.Gfx().ClearBuffer( c,c,1.0f );
 	wnd.Gfx().DrawTestTriangle(
-		-timer.Peek(),
-		0.0f,
-		0.0f
-	);
-	wnd.Gfx().DrawTestTriangle(
 		timer.Peek(),
 		wnd.mouse.GetPosX() / 400.0f - 1.0f,
 		-wnd.mouse.GetPosY() / 300.0f + 1.0f
