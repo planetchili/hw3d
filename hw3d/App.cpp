98fa77f0379ebd9ead1a367eedbc6d43e88de1cb
chili <ray_kun@hotmail.com>
2019-03-03 19:16:04 +0900

    remove useless textinput

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -88,14 +88,12 @@ void App::DoFrame()
 		d->Draw( wnd.Gfx() );
 	}
 
-	static char buffer[1024];
-
 	// imgui window to control simulation speed
 	if( ImGui::Begin( "Simulation Speed" ) )
 	{
 		ImGui::SliderFloat( "Speed Factor",&speed_factor,0.0f,4.0f );
-		ImGui::Text( "Application average %.3f ms/frame (%.1f FPS)",1000.0f / ImGui::GetIO().Framerate,ImGui::GetIO().Framerate );
-		ImGui::InputText( "Butts",buffer,sizeof( buffer ) );
+		ImGui::Text( "%.3f ms/frame (%.1f FPS)",1000.0f / ImGui::GetIO().Framerate,ImGui::GetIO().Framerate );
+		ImGui::Text( "Status: %s",wnd.kbd.KeyIsPressed( VK_SPACE ) ? "PAUSED" : "RUNNING" );
 	}
 	ImGui::End();
 
