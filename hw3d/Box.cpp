92780ee344fb57b7bc5109b9ee407af85d05ef96
chili <ray_kun@hotmail.com>
2019-04-14 15:06:35 +0900

    problem with specular

--- hw3d/Box.cpp
+++ hw3d/Box.cpp
@@ -64,8 +64,10 @@ Box::Box( Graphics& gfx,
 	
 	struct PSMaterialConstant
 	{
-		dx::XMFLOAT3 color;
-		float padding;
+		alignas(16) dx::XMFLOAT3 color;
+		float specularIntensity = 0.6f;
+		float specularPower = 30.0f;
+		float padding[2];
 	} colorConst;
 	colorConst.color = material;
 	AddBind( std::make_unique<PixelConstantBuffer<PSMaterialConstant>>( gfx,colorConst,1u ) );
--- hw3d/PhongPS.hlsl
+++ hw3d/PhongPS.hlsl
@@ -12,6 +12,8 @@ cbuffer LightCBuf
 cbuffer ObjectCBuf
 {
 	float3 materialColor;
+	float specularIntensity;
+	float specularPower;
 };
 
 
@@ -25,6 +27,11 @@ float4 main( float3 worldPos : Position,float3 n : Normal ) : SV_Target
 	const float att = 1.0f / (attConst + attLin * distToL + attQuad * (distToL * distToL));
 	// diffuse intensity
 	const float3 diffuse = diffuseColor * diffuseIntensity * att * max( 0.0f,dot( dirToL,n ) );
+	// reflected light vector
+	const float3 w = n * dot( dirToL,n );
+	const float3 r = w * 2.0f - dirToL;
+	// calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
+	const float3 specular = (diffuseColor * diffuseIntensity) * specularIntensity * pow( max( 0.0f,dot( normalize( r ),normalize( worldPos ) ) ),specularPower );
 	// final color
-	return float4(saturate( (diffuse + ambient) * materialColor ),1.0f);
+	return float4(saturate( (diffuse + ambient + specular) * materialColor ),1.0f);
 }
