3bff8c88c5fd7b32472bfb46fc610f75c234ac44
chili <ray_kun@hotmail.com>
2019-04-14 23:25:44 +0900

    probably a good idea to attenuate the specular as well

--- hw3d/PhongPS.hlsl
+++ hw3d/PhongPS.hlsl
@@ -23,7 +23,7 @@ float4 main( float3 worldPos : Position,float3 n : Normal ) : SV_Target
 	const float3 vToL = lightPos - worldPos;
 	const float distToL = length( vToL );
 	const float3 dirToL = vToL / distToL;
-	// diffuse attenuation
+	// attenuation
 	const float att = 1.0f / (attConst + attLin * distToL + attQuad * (distToL * distToL));
 	// diffuse intensity
 	const float3 diffuse = diffuseColor * diffuseIntensity * att * max( 0.0f,dot( dirToL,n ) );
@@ -31,7 +31,7 @@ float4 main( float3 worldPos : Position,float3 n : Normal ) : SV_Target
     const float3 w = n * dot( vToL,n );
     const float3 r = w * 2.0f - vToL;
 	// calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
-	const float3 specular = (diffuseColor * diffuseIntensity) * specularIntensity * pow( max( 0.0f,dot( normalize( -r ),normalize( worldPos ) ) ),specularPower );
+	const float3 specular = att * (diffuseColor * diffuseIntensity) * specularIntensity * pow( max( 0.0f,dot( normalize( -r ),normalize( worldPos ) ) ),specularPower );
 	// final color
 	return float4(saturate( (diffuse + ambient + specular) * materialColor ),1.0f);
 }
