83418ac309defc1710c6a4132a9c5eab9736836e
chili <ray_kun@hotmail.com>
2019-05-03 19:55:02 +0900

    fix flickering bug

--- hw3d/Box.cpp
+++ hw3d/Box.cpp
@@ -76,9 +76,10 @@ void Box::SpawnControlWindow( int id,Graphics& gfx ) noexcept
 	bool dirty = false;
 	if( ImGui::Begin( ("Box "s + std::to_string( id )).c_str() ) )
 	{
-		dirty = dirty || ImGui::ColorEdit3( "Material Color",&materialConstants.color.x );
-		dirty = dirty || ImGui::SliderFloat( "Specular Intensity",&materialConstants.specularIntensity,0.05f,4.0f,"%.2f",2 );
-		dirty = dirty || ImGui::SliderFloat( "Specular Power",&materialConstants.specularPower,1.0f,200.0f,"%.2f",2 );
+		const auto cd = ImGui::ColorEdit3( "Material Color",&materialConstants.color.x );
+		const auto sid = ImGui::SliderFloat( "Specular Intensity",&materialConstants.specularIntensity,0.05f,4.0f,"%.2f",2 );
+		const auto spd = ImGui::SliderFloat( "Specular Power",&materialConstants.specularPower,1.0f,200.0f,"%.2f",2 );
+		dirty = cd || sid || spd;
 	}
 	ImGui::End();
 
