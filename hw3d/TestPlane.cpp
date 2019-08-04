9f94557f633902c5fe4f1fd8e3a5c85eda525cfb
chili <ray_kun@hotmail.com>
2019-08-04 18:00:30 +0900

    non-retarded specular parameters for brick

--- hw3d/TestPlane.cpp
+++ hw3d/TestPlane.cpp
@@ -23,8 +23,8 @@ TestPlane::TestPlane( Graphics& gfx,float size )
 
 	struct PSMaterialConstant
 	{
-		float specularIntensity = 0.8f;
-		float specularPower = 45.0f;
+		float specularIntensity = 0.1f;
+		float specularPower = 20.0f;
 		float padding[2];
 	} pmc;
 	AddBind( PixelConstantBuffer<PSMaterialConstant>::Resolve( gfx,pmc,1u ) );
