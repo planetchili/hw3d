51a0c50e151e3bfaed3697cec24f5e4a49be885c
chili <ray_kun@hotmail.com>
2019-08-27 21:37:06 +0900

    create bindable for dual vtx/pxl transform binding

---
+++ hw3d/TransformCbufDoubleboi.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "TransformCbuf.h"
+
+namespace Bind
+{
+	class TransformCbufDoubleboi : public TransformCbuf
+	{
+	public:
+		TransformCbufDoubleboi( Graphics& gfx,const Drawable& parent,UINT slotV = 0u,UINT slotP = 0u );
+		void Bind( Graphics& gfx ) noexcept override;
+	protected:
+		void UpdateBindImpl( Graphics& gfx,const Transforms& tf ) noexcept;
+	private:
+		static std::unique_ptr<PixelConstantBuffer<Transforms>> pPcbuf;
+	};
+}
