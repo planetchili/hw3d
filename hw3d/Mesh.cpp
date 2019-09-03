9f55b8a6e38604e90df41aa05e93e3bf59103482
chili <ray_kun@hotmail.com>
2019-09-03 16:21:42 +0900

    node manipulation system reads initial node transforms

--- hw3d/Mesh.cpp
+++ hw3d/Mesh.cpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <sstream>
 #include <filesystem>
+#include "ChiliXM.h"
 
 namespace dx = DirectX;
 
@@ -121,6 +122,11 @@ void Node::SetAppliedTransform( DirectX::FXMMATRIX transform ) noexcept
 	dx::XMStoreFloat4x4( &appliedTransform,transform );
 }
 
+const DirectX::XMFLOAT4X4& Node::GetAppliedTransform() const noexcept
+{
+	return appliedTransform;
+}
+
 int Node::GetId() const noexcept
 {
 	return id;
@@ -145,7 +151,23 @@ public:
 			ImGui::NextColumn();
 			if( pSelectedNode != nullptr )
 			{
-				auto& transform = transforms[pSelectedNode->GetId()];
+				const auto id = pSelectedNode->GetId();
+				auto i = transforms.find( id );
+				if( i == transforms.end() )
+				{
+					const auto& applied = pSelectedNode->GetAppliedTransform();
+					const auto angles = ExtractEulerAngles( applied );
+					const auto translation = ExtractTranslation( applied );
+					TransformParameters tp;
+					tp.roll = angles.z;
+					tp.pitch = angles.x;
+					tp.yaw = angles.y;
+					tp.x = translation.x;
+					tp.y = translation.y;
+					tp.z = translation.z;
+					std::tie( i,std::ignore ) = transforms.insert( { id,tp } );
+				}
+				auto& transform = i->second;
 				ImGui::Text( "Orientation" );
 				ImGui::SliderAngle( "Roll",&transform.roll,-180.0f,180.0f );
 				ImGui::SliderAngle( "Pitch",&transform.pitch,-180.0f,180.0f );
--- hw3d/Mesh.h
+++ hw3d/Mesh.h
@@ -58,6 +58,7 @@ public:
 	Node( int id,const std::string& name,std::vector<Mesh*> meshPtrs,const DirectX::XMMATRIX& transform ) noxnd;
 	void Draw( Graphics& gfx,DirectX::FXMMATRIX accumulatedTransform ) const noxnd;
 	void SetAppliedTransform( DirectX::FXMMATRIX transform ) noexcept;
+	const DirectX::XMFLOAT4X4& GetAppliedTransform() const noexcept;
 	int GetId() const noexcept;
 	void ShowTree( Node*& pSelectedNode ) const noexcept;
 	template<class T>
