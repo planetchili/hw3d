721391a62d4a7b71395620b5ee2dffbdd3fd3d46
chili <ray_kun@hotmail.com>
2019-09-19 06:01:16 +0900

    need to set lod on sampler to use mipmapping

--- hw3d/Sampler.cpp
+++ hw3d/Sampler.cpp
@@ -13,6 +13,9 @@ namespace Bind
 		samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 		samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 		samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+		samplerDesc.MipLODBias = 0.0f;
+		samplerDesc.MinLOD = 0.0f;
+		samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
 
 		GFX_THROW_INFO( GetDevice( gfx )->CreateSamplerState( &samplerDesc,&pSampler ) );
 	}
