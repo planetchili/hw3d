b90d65b08624f641a44ed25d530a66fc4f2fb667
chili <ray_kun@hotmail.com>
2019-10-26 01:51:43 +0900

    improve blending dark band with max()

--- hw3d/Blur_PS.hlsl
+++ hw3d/Blur_PS.hlsl
@@ -10,14 +10,17 @@ float4 main(float2 uv : Texcoord) : SV_Target
     tex.GetDimensions(width, height);
     const float dx = 1.0f / width;
     const float dy = 1.0f / height;
-    float4 acc = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float accAlpha = 0.0f;
+    float3 maxColor = float3(0.0f, 0.0f, 0.0f);
     for (int y = -r; y <= r; y++)
     {
         for (int x = -r; x <= r; x++)
         {
             const float2 tc = uv + float2(dx * x, dy * y);
-            acc += tex.Sample(splr, tc).rgba;
+            const float4 s = tex.Sample(splr, tc).rgba;
+            accAlpha += s.a;
+            maxColor = max(s.rgb, maxColor);
         }
     }
-    return acc / divisor;
+    return float4(maxColor, accAlpha / divisor);
 }
