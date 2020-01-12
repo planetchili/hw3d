3a799be7d8b23f2221d599c13755c84a41d8128d
chili <ray_kun@hotmail.com>
2020-01-12 09:35:12 +0900

    better alignment on perflog

--- hw3d/PerfLog.h
+++ hw3d/PerfLog.h
@@ -19,13 +19,14 @@ private:
 		float time;
 		void WriteTo( std::ostream& out ) const noexcept
 		{
+			using namespace std::string_literals;
 			if( label.empty() )
 			{
 				out << time * 1000.0f << "ms\n";
 			}
 			else
 			{
-				out << "[" << label << "] " << time * 1000.0f << "ms\n";
+				out << std::setw( 16 ) << std::left << "["s + label + "] "s << std::setw(6) << std::right << time * 1000.0f << "ms\n";
 			}
 		}
 	};
