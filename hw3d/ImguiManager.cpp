8e387bea2ac5d56e1dd0c5360614bdecbbbc7c5e
chili <ray_kun@hotmail.com>
2020-01-19 14:13:38 +0900

    imgui window default config commitable

--- hw3d/ImguiManager.cpp
+++ hw3d/ImguiManager.cpp
@@ -1,8 +1,15 @@
 #include "ImguiManager.h"
 #include "imgui/imgui.h"
+#include <filesystem>
 
 ImguiManager::ImguiManager()
 {
+	namespace fs = std::filesystem;
+	if( !fs::exists( "imgui.ini" ) && fs::exists( "imgui_default.ini" ) )
+	{
+		fs::copy_file( "imgui_default.ini","imgui.ini" );
+	}
+
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGui::StyleColorsDark();
