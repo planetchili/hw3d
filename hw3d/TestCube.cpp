d6f988d30d0c1c6eec60b4ee6f7264bdb8f5d50e
chili <ray_kun@hotmail.com>
2020-03-04 21:53:39 +0900

    this time shader stuff fixed for sure ;p

--- hw3d/TestCube.cpp
+++ hw3d/TestCube.cpp
@@ -28,18 +28,20 @@ TestCube::TestCube( Graphics& gfx,float size )
 			only.AddBindable( Texture::Resolve( gfx,"Images\\brickwall.jpg" ) );
 			only.AddBindable( Sampler::Resolve( gfx ) );
 
-			auto pvs = VertexShader::Resolve( gfx,"Phong_VS.cso" );
+			auto pvs = VertexShader::Resolve( gfx,"PhongDif_VS.cso" );
 			auto pvsbc = pvs->GetBytecode();
 			only.AddBindable( std::move( pvs ) );
 
-			only.AddBindable( PixelShader::Resolve( gfx,"Phong_PS.cso" ) );
+			only.AddBindable( PixelShader::Resolve( gfx,"PhongDif_PS.cso" ) );
 			
 			Dcb::RawLayout lay;
-			lay.Add<Dcb::Float>( "specularIntensity" );
-			lay.Add<Dcb::Float>( "specularPower" );
+			lay.Add<Dcb::Float3>( "specularColor" );
+			lay.Add<Dcb::Float>( "specularWeight" );
+			lay.Add<Dcb::Float>( "specularGloss" );
 			auto buf = Dcb::Buffer( std::move( lay ) );
-			buf["specularIntensity"] = 0.1f;
-			buf["specularPower"] = 20.0f;
+			buf["specularColor"] = dx::XMFLOAT3{ 1.0f,1.0f,1.0f };
+			buf["specularWeight"] = 0.1f;
+			buf["specularGloss"] = 20.0f;
 			only.AddBindable( std::make_shared<Bind::CachingPixelConstantBufferEx>( gfx,buf,1u ) );
 
 			only.AddBindable( InputLayout::Resolve( gfx,model.vertices.GetLayout(),pvsbc ) );
@@ -56,7 +58,7 @@ TestCube::TestCube( Graphics& gfx,float size )
 		{
 			Step mask( 1 );
 
-			auto pvs = VertexShader::Resolve( gfx,"SolidVS.cso" );
+			auto pvs = VertexShader::Resolve( gfx,"Solid_VS.cso" );
 			auto pvsbc = pvs->GetBytecode();
 			mask.AddBindable( std::move( pvs ) );
 
@@ -73,12 +75,12 @@ TestCube::TestCube( Graphics& gfx,float size )
 			Step draw( 2 );
 
 			// these can be pass-constant (tricky due to layout issues)
-			auto pvs = VertexShader::Resolve( gfx,"SolidVS.cso" );
+			auto pvs = VertexShader::Resolve( gfx,"Solid_VS.cso" );
 			auto pvsbc = pvs->GetBytecode();
 			draw.AddBindable( std::move( pvs ) );
 
 			// this can be pass-constant
-			draw.AddBindable( PixelShader::Resolve( gfx,"SolidPS.cso" ) );
+			draw.AddBindable( PixelShader::Resolve( gfx,"Solid_PS.cso" ) );
 
 			Dcb::RawLayout lay;
 			lay.Add<Dcb::Float4>( "color" );
