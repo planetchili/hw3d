733d7249ecb6fb10c0fe7027ad14cf20ea67bd6f
chili <ray_kun@hotmail.com>
2020-04-02 01:10:07 +0900

    more checks for RG compilation

--- hw3d/RenderGraph.cpp
+++ hw3d/RenderGraph.cpp
@@ -118,14 +118,22 @@ namespace Rgph
 			}
 			else // find source from within existing passes
 			{
+				bool bound = false;
 				for( auto& existingPass : passes )
 				{
 					if( existingPass->GetName() == inputSourcePassName )
 					{
 						auto& source = existingPass->GetSource( si->GetOutputName() );
 						si->Bind( source );
+						bound = true;
 						break;
 					}
+				}
+				if( !bound )
+				{
+					std::ostringstream oss;
+					oss << "Pass named [" << inputSourcePassName << "] not found";
+					throw RGC_EXCEPTION( oss.str() );
 				}
 			}
 		}
