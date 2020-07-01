ef524c45c25e8be70c01f9173fb893f17b0a9a9d
chili <ray_kun@hotmail.com>
2020-07-01 12:51:33 +0900

    initial changes for selectable skysphere

--- hw3d/SkyboxPass.h
+++ hw3d/SkyboxPass.h
@@ -38,14 +38,13 @@ namespace Rgph
 			AddBind( std::make_shared<SkyboxTransformCbuf>( gfx ) );
 			AddBind( PixelShader::Resolve( gfx,"Skybox_PS.cso" ) );
 			AddBind( Topology::Resolve( gfx,D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST ) );
-			{
+			{ // geometry-related
 				auto pvs = Bind::VertexShader::Resolve( gfx,"Skybox_VS.cso" );
 				auto model = Cube::Make();
-				model.Transform( DirectX::XMMatrixScaling( 3.0f,3.0f,3.0f ) );
 				const auto geometryTag = "$cube_map";
-				AddBind( VertexBuffer::Resolve( gfx,geometryTag,std::move( model.vertices ) ) );
-				count = (UINT)model.indices.size();
-				AddBind( IndexBuffer::Resolve( gfx,geometryTag,std::move( model.indices ) ) );
+				pCubeVertices = VertexBuffer::Resolve( gfx,geometryTag,std::move( model.vertices ) );
+				cubeCount = (UINT)model.indices.size();
+				pCubeIndices = IndexBuffer::Resolve( gfx,geometryTag,std::move( model.indices ) );
 				AddBind( InputLayout::Resolve( gfx,model.vertices.GetLayout(),*pvs ) );
 				AddBind( std::move( pvs ) );
 			}
@@ -60,11 +59,19 @@ namespace Rgph
 		{
 			assert( pMainCamera );
 			pMainCamera->BindToGraphics( gfx );
-			BindAll( gfx );
-			gfx.DrawIndexed( count );
+			pCubeVertices->Bind( gfx );
+			pCubeIndices->Bind( gfx );
+			BindAll( gfx ); 
+			gfx.DrawIndexed( cubeCount );
 		}
 	private:
-		UINT count;
+		bool useSphere = false;
 		const Camera* pMainCamera = nullptr;
+		std::shared_ptr<Bind::VertexBuffer> pCubeVertices;
+		std::shared_ptr<Bind::IndexBuffer> pCubeIndices;
+		UINT cubeCount;
+		std::shared_ptr<Bind::VertexBuffer> pSphereVertices;
+		std::shared_ptr<Bind::IndexBuffer> pSphereIndices;
+		UINT sphereCount;
 	};
 }
