621794e6acc2a76ad867d49224596a76f72c5208
chili <ray_kun@hotmail.com>
2020-08-10 20:09:51 +0900

    shadow fov perspective

--- hw3d/ShadowMappingPass.h
+++ hw3d/ShadowMappingPass.h
@@ -13,6 +13,7 @@
 #include "ShadowRasterizer.h"
 #include "CubeTexture.h"
 #include "Viewport.h"
+#include "ChiliMath.h"
 
 
 class Graphics;
@@ -42,7 +43,7 @@ namespace Rgph
 
 			DirectX::XMStoreFloat4x4(
 				&projection,
-				DirectX::XMMatrixPerspectiveLH( 1.0f,1.0f,0.5f,100.0f )
+				DirectX::XMMatrixPerspectiveFovLH( PI / 2.0f,1.0f,0.5f,100.0f )
 			);
 			// +x
 			DirectX::XMStoreFloat3( &cameraDirections[0],DirectX::XMVectorSet( 1.0f,0.0f,0.0f,0.0f ) );
