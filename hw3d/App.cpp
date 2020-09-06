56118c9b6e9a060f0bbaede5e0f9d6766c1cf060
chili <ray_kun@hotmail.com>
2020-09-06 13:07:56 +0900

    restore the full scene

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -20,26 +20,26 @@ App::App( const std::string& commandLine )
 	light( wnd.Gfx(),{ 10.0f,5.0f,0.0f } )
 {
 	cameras.AddCamera( std::make_unique<Camera>( wnd.Gfx(),"A",dx::XMFLOAT3{ -13.5f,6.0f,3.5f },0.0f,PI / 2.0f ) );
-	//cameras.AddCamera( std::make_unique<Camera>( wnd.Gfx(),"B",dx::XMFLOAT3{ -13.5f,28.8f,-6.4f },PI / 180.0f * 13.0f,PI / 180.0f * 61.0f ) );
-	//cameras.AddCamera( light.ShareCamera() );
+	cameras.AddCamera( std::make_unique<Camera>( wnd.Gfx(),"B",dx::XMFLOAT3{ -13.5f,28.8f,-6.4f },PI / 180.0f * 13.0f,PI / 180.0f * 61.0f ) );
+	cameras.AddCamera( light.ShareCamera() );
 
 	cube.SetPos( { 10.0f,5.0f,6.0f } );
-	//cube2.SetPos( { 10.0f,5.0f,14.0f } );
-	//nano.SetRootTransform(
-	//	dx::XMMatrixRotationY( PI / 2.f ) *
-	//	dx::XMMatrixTranslation( 27.f,-0.56f,1.7f )
-	//);
-	//gobber.SetRootTransform(
-	//	dx::XMMatrixRotationY( -PI / 2.f ) *
-	//	dx::XMMatrixTranslation( -8.f,10.f,0.f )
-	//);
+	cube2.SetPos( { 10.0f,5.0f,14.0f } );
+	nano.SetRootTransform(
+		dx::XMMatrixRotationY( PI / 2.f ) *
+		dx::XMMatrixTranslation( 27.f,-0.56f,1.7f )
+	);
+	gobber.SetRootTransform(
+		dx::XMMatrixRotationY( -PI / 2.f ) *
+		dx::XMMatrixTranslation( -8.f,10.f,0.f )
+	);
 	
 	cube.LinkTechniques( rg );
-	//cube2.LinkTechniques( rg );
+	cube2.LinkTechniques( rg );
 	light.LinkTechniques( rg );
-	//sponza.LinkTechniques( rg );
-	//gobber.LinkTechniques( rg );
-	//nano.LinkTechniques( rg );
+	sponza.LinkTechniques( rg );
+	gobber.LinkTechniques( rg );
+	nano.LinkTechniques( rg );
 	cameras.LinkTechniques( rg );
 
 	rg.BindShadowCamera( *light.ShareCamera() );
@@ -122,18 +122,18 @@ void App::DoFrame( float dt )
 		
 	light.Submit( Chan::main );
 	cube.Submit( Chan::main );
-	//sponza.Submit( Chan::main );
-	//cube2.Submit( Chan::main );
-	//gobber.Submit( Chan::main );
-	//nano.Submit( Chan::main );
+	sponza.Submit( Chan::main );
+	cube2.Submit( Chan::main );
+	gobber.Submit( Chan::main );
+	nano.Submit( Chan::main );
 	cameras.Submit( Chan::main );
 
-	//sponza.Submit( Chan::shadow );
+	sponza.Submit( Chan::shadow );
 	cube.Submit( Chan::shadow );
-	//sponza.Submit( Chan::shadow );
-	//cube2.Submit( Chan::shadow );
-	//gobber.Submit( Chan::shadow );
-	//nano.Submit( Chan::shadow );
+	sponza.Submit( Chan::shadow );
+	cube2.Submit( Chan::shadow );
+	gobber.Submit( Chan::shadow );
+	nano.Submit( Chan::shadow );
 
 	rg.Execute( wnd.Gfx() );
 
@@ -147,14 +147,14 @@ void App::DoFrame( float dt )
 	static MP sponzeProbe{ "Sponza" };
 	static MP gobberProbe{ "Gobber" };
 	static MP nanoProbe{ "Nano" };
-	//sponzeProbe.SpawnWindow( sponza );
-	//gobberProbe.SpawnWindow( gobber );
-	//nanoProbe.SpawnWindow( nano );
+	sponzeProbe.SpawnWindow( sponza );
+	gobberProbe.SpawnWindow( gobber );
+	nanoProbe.SpawnWindow( nano );
 	cameras.SpawnWindow( wnd.Gfx() );
 	light.SpawnControlWindow();
 	ShowImguiDemoWindow();
 	cube.SpawnControlWindow( wnd.Gfx(),"Cube 1" );
-	//cube2.SpawnControlWindow( wnd.Gfx(),"Cube 2" );
+	cube2.SpawnControlWindow( wnd.Gfx(),"Cube 2" );
 	
 	rg.RenderWindows( wnd.Gfx() );
 
--- hw3d/App.h
+++ hw3d/App.h
@@ -32,10 +32,10 @@ private:
 	float speed_factor = 1.0f;
 	CameraContainer cameras;
 	PointLight light;
-	TestCube cube{ wnd.Gfx(),14.0f };
-	//TestCube cube2{ wnd.Gfx(),4.0f };
-	//Model sponza{ wnd.Gfx(),"Models\\sponza\\sponza.obj",1.0f / 20.0f };
-	//Model gobber{ wnd.Gfx(),"Models\\gobber\\GoblinX.obj",4.0f };
-	//Model nano{ wnd.Gfx(),"Models\\nano_textured\\nanosuit.obj",2.0f };
+	TestCube cube{ wnd.Gfx(),4.0f };
+	TestCube cube2{ wnd.Gfx(),4.0f };
+	Model sponza{ wnd.Gfx(),"Models\\sponza\\sponza.obj",1.0f / 20.0f };
+	Model gobber{ wnd.Gfx(),"Models\\gobber\\GoblinX.obj",4.0f };
+	Model nano{ wnd.Gfx(),"Models\\nano_textured\\nanosuit.obj",2.0f };
 	bool savingDepth = false;
 };
